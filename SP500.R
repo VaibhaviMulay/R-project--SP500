5de3b2c34f11177c1025f8426b3034c7b7964f87
Vaibhavi Mulay <38724030+VaibhaviMulay@users.noreply.github.com>
2020-03-22 23:18:05 -0400

    .

--- SP500.R
+++ SP500.R
@@ -8,12 +8,33 @@ library(docstring)
 library(readr)
 library(here)
 library(data.table)
+library(urca)
 
 data_master = copy(dadm_data)
 data_master
 
 attach(dadm_data)
 data_master
+
+# Doing Kpss test to find out stationary or not
+grep('NA',data_master$Open)     #finding if any NA values or else the code won't run having NA values
+Test<- ur.kpss(data_master$Open)    #kpss testing
+summary(Test)
+# from the summary we can see that the t-stat value is bigger than 1% and 5% critical value indicating that the null hypothesis is rejected.
+plot(Test)
+
+
+# NOW? WHAT IS THE SOLUTION TO THIS??
+# OKAY SO WE NEED TO DIFFERENTIATE THE DATA AND APPLY THE KPSS TEST AGAIN TO CHECK IF STATIONARY OR NOT !
+
+#Difference the data
+diff(data_master$Open)
+
+Test2=ur.kpss(diff(data_master$Open)) 
+summary(Test2)
+
+# NOTICE THE DIFFERENCE? NOW YOU CAN SEE THAT THE T-STAT VALUE IS WAY SMALLER THAN THE 1% and 5% CRITICAL VALUES ! 
+# THAT INDICATES THAT THE DATA IS NOW STATIONARY !
 
 
 sp_500 = ts(data_master$Open, start=c(1995, 1),end = c(2017,3), freq=12)
@@ -24,12 +45,10 @@ plot(sp_500)
 Box.test(sp_500, lag = 20, type = 'Ljung-Box')
 
 adf.test(sp_500)
- sp_500_training = ts(data_master$Open, start=c(1995, 1),end = c(2014,12), freq=12)
+sp_500_training = ts(data_master$Open, start=c(1995, 1),end = c(2014,12), freq=12)
 
- Box.test(sp_500_training, lag = 20, type = 'Ljung-Box')
+Box.test(sp_500_training, lag = 20, type = 'Ljung-Box')
 adf.test(sp_500_training)        
-
-plot_decomp(sp_500_training)
 
 decomp = decompose(sp_500_training)
 plot(decomp)
@@ -39,16 +58,19 @@ plot(seasonalAdjusted)
 
 acf = acf(sp_500_training)
 plot(acf)
-pacf(sp_500_training)
+pacf = pacf(sp_500_training)
+plot(pacf)
 
 tsdiff = diff(sp_500_training)
 plot(tsdiff)
 
-acf(tsdiff)
-pacf(tsdiff)
+acftsdiff = acf(tsdiff)
+pacftsdiff = pacf(tsdiff)
+plot(acftsdiff)
+plot(pacftsdiff)
 
-auto.arima()
-fit <- Arima(sp_500_training, order = c(0,1,1),include.drift = TRUE)
+auto.arima(sp_500_training)
+fit <- Arima(sp_500_training, order = c(0,1,0),include.drift = TRUE)
 summary(fit)
 
 
@@ -78,17 +100,26 @@ plot(sp_500_all)
 
 round(accuracy(f = sp_500_all, x = sp_500_test, test = NULL,d =NULL,D = NULL), 3)
 
-aa = snaive(sp_500_training,h=27)
-aa1 = forecast(aa,h=12)
-round(accuracy(f = aa1, x = sp_500_test, test = NULL,d =NULL,D = NULL), 3)
+snaiveTraining = snaive(sp_500_training,h=27)
+forecastedSnaive = forecast(snaiveTraining,h=12)
+round(accuracy(f = forecastedsnaive, x = sp_500_test, test = NULL,d =NULL,D = NULL), 3)
 
-bb = meanf(sp_500_training,h=27)
-bb1 = forecast(bb,h=27)
-round(accuracy(f = bb1, x = sp_500_test, test = NULL,d =NULL,D = NULL), 3)
+meanfTraining = meanf(sp_500_training,h=27)
+forecastedMeanf = forecast(meanfTraining,h=27)
+round(accuracy(f = forecastedMeanf, x = sp_500_test, test = NULL,d =NULL,D = NULL), 3)
 
-cc = naive(sp_500_training,h=27)
-cc1 = forecast(cc,h=27)
-round(accuracy(f = cc1, x = sp_500_test, test = NULL,d =NULL,D = NULL), 3)
+naiveTraining = naive(sp_500_training,h=27)
+forcastedNaive = forecast(naiveTraining,h=27)
+round(accuracy(f = forcastedNaive, x = sp_500_test, test = NULL,d =NULL,D = NULL), 3)
 
+train <- sp_500_training
+test <- sp_500_test
 
+ARIMA <-  as.ts(sp_500_all)[, 1]
+snaive <- as.ts(forecastedSnaive)[, 1]
+meanf <- as.ts(forecastedMeanf)[, 1]
+naive <- as.ts(forcastedNaive)[, 1]
 
+AP <- cbind(sp_500, train, test, 
+            ARIMA, snaive, meanf, naive)
+autoplot(AP)
